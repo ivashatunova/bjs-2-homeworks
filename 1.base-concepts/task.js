9c6c758d5db0553da2ac2d535979d53d3f6bbf35
i.shatunova <ivashatunova@gmail.com>
2023-07-06 23:40:59 +0300

    Second task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,5 +18,40 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+   // Проверяем, являются ли аргументы числами или могут быть преобразованы в числа
+   if (
+    typeof percent !== 'number' ||
+    typeof contribution !== 'number' ||
+    typeof amount !== 'number' ||
+    typeof countMonths !== 'number' ||
+    isNaN(percent) ||
+    isNaN(contribution) ||
+    isNaN(amount) ||
+    isNaN(countMonths)
+  ) {
+    return false;
+  }
+
+  // Преобразуем процентную ставку в десятичное число
+  let interestRate = percent / 100;
+  // Преобразуем годовую процентную ставку в месячную
+  let monthlyRate = interestRate / 12;
+
+  // Вычисляем тело кредита
+  let loanAmount = amount - contribution;
+
+  // Рассчитываем ежемесячный платеж
+  let monthlyPayment =
+    loanAmount *
+    (monthlyRate +
+      monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1));
+
+  // Вычисляем общую сумму, которую придется заплатить клиенту
+  let totalPayment = monthlyPayment * countMonths;
+
+  // Округляем результат до двух значений после запятой
+  let roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+
+  return roundedTotalPayment;
 }
+
