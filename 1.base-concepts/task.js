bf60cfe52374a778badff85e42669d8372eb5276
i.shatunova <ivashatunova@gmail.com>
2023-07-06 22:56:27 +0300

    First task done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2-4*a*c;
+
+  if (d < 0) {
+    return arr;
+  } else if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else {
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(root1, root2);
+  }
   
   return arr;
 }
