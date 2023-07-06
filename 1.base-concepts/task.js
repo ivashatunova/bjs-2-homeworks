a3b16f017bd5e13e16a8e1e4e9c35009c4b896c8
i.shatunova <ivashatunova@gmail.com>
2023-07-06 23:47:46 +0300

    Reformat code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,57 +1,57 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b**2-4*a*c;
 
-  if (d < 0) {
-    return arr;
-  } else if (d === 0) {
-    let root = -b / (2 * a);
-    arr.push(root);
-  } else {
-    let root1 = (-b + Math.sqrt(d)) / (2 * a);
-    let root2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(root1, root2);
-  }
-  
-  return arr;
+function solveEquation(a, b, c) {
+	let arr = [];
+	let d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+	} else {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-   // Проверяем, являются ли аргументы числами или могут быть преобразованы в числа
-   if (
-    typeof percent !== 'number' ||
-    typeof contribution !== 'number' ||
-    typeof amount !== 'number' ||
-    typeof countMonths !== 'number' ||
-    isNaN(percent) ||
-    isNaN(contribution) ||
-    isNaN(amount) ||
-    isNaN(countMonths)
-  ) {
-    return false;
-  }
+	// Проверяем, являются ли аргументы числами или могут быть преобразованы в числа
+	if (
+		typeof percent !== 'number' ||
+		typeof contribution !== 'number' ||
+		typeof amount !== 'number' ||
+		typeof countMonths !== 'number' ||
+		isNaN(percent) ||
+		isNaN(contribution) ||
+		isNaN(amount) ||
+		isNaN(countMonths)
+	) {
+		return false;
+	}
 
-  // Преобразуем процентную ставку в десятичное число
-  let interestRate = percent / 100;
-  // Преобразуем годовую процентную ставку в месячную
-  let monthlyRate = interestRate / 12;
+	// Преобразуем процентную ставку в десятичное число
+	let interestRate = percent / 100;
+	// Преобразуем годовую процентную ставку в месячную
+	let monthlyRate = interestRate / 12;
 
-  // Вычисляем тело кредита
-  let loanAmount = amount - contribution;
+	// Вычисляем тело кредита
+	let loanAmount = amount - contribution;
 
-  // Рассчитываем ежемесячный платеж
-  let monthlyPayment =
-    loanAmount *
-    (monthlyRate +
-      monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1));
+	// Рассчитываем ежемесячный платеж
+	let monthlyPayment =
+		loanAmount *
+		(monthlyRate +
+			monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1));
 
-  // Вычисляем общую сумму, которую придется заплатить клиенту
-  let totalPayment = monthlyPayment * countMonths;
+	// Вычисляем общую сумму, которую придется заплатить клиенту
+	let totalPayment = monthlyPayment * countMonths;
 
-  // Округляем результат до двух значений после запятой
-  let roundedTotalPayment = Math.round(totalPayment * 100) / 100;
+	// Округляем результат до двух значений после запятой
+	let roundedTotalPayment = Math.round(totalPayment * 100) / 100;
 
-  return roundedTotalPayment;
+	return roundedTotalPayment;
 }
-
