d05c06ee2b9bcb9fed83d1d649c65999d3a2b1fc
i.shatunova <ivashatunova@gmail.com>
2023-07-11 23:56:27 +0300

    Second task done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,31 @@
 function compareArrays(arr1, arr2) {
-    return arr1.every((element, index) => {
-        console.log(element, index)
-        if (element === arr2[index]) {
-            return true;
-        }
-        else { 
-            return false; 
-        }
-    })
-}
+    if (arr1.length != arr2.length) {
+        return false;
+    }
+    else {
+        return arr1.every((element, index) => {
+            console.log(element, index)
+            if (element === arr2[index]) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        })
+    }
 
-console.log(compareArrays([8, 5, 2], [8, 1, 2]));
+}
 
 function getUsersNamesInAgeRange(users, gender) {
+    if (users.length === 0) {
+        return 0
+    }
+    else {
+        return result = users
+            .filter(user => user.gender === gender)
+            .map(user => user.age)
+            .reduce((sum, age, index, array) => sum + age / array.length, 0)
+    }
 
 }
+
