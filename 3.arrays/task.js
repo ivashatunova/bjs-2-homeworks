f1e41ca76e119f23ab27ea4ce6552f77c83e767c
i.shatunova <ivashatunova@gmail.com>
2023-07-11 15:02:08 +0300

    First  task done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((element, index) => {
+        console.log(element, index)
+        if (element === arr2[index]) {
+            return true;
+        }
+        else { 
+            return false; 
+        }
+    })
 }
 
+console.log(compareArrays([8, 5, 2], [8, 1, 2]));
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
 }
