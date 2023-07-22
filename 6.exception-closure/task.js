688a931c9f6396dda33ecfc3abceddab01163346
i.shatunova <ivashatunova@gmail.com>
2023-07-22 07:53:31 +0300

    First task done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,20 @@
-﻿
+﻿function parseCount(number) {
+    const parsedValue = Number.parseFloat(number);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+
+
+function validateCount(number) {
+    try {
+        return parseCount(number)
+    } catch (error) {
+        return error;
+    }
+}
+
+
+
