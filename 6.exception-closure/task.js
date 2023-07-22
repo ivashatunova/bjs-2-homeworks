16f0dc09cd7a64e8d80244d7a5c000c049fde0da
i.shatunova <ivashatunova@gmail.com>
2023-07-22 19:04:05 +0300

    Second task done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,5 +16,47 @@ function validateCount(number) {
     }
 }
 
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    get perimeter() {
+        const perimeter = this.a + this.b + this.c;
+        return parseFloat(perimeter);
+
+    }
+
+    get area() {
+        const halfPerimeter = this.perimeter / 2;
+        const area = Math.sqrt((halfPerimeter) * (halfPerimeter - this.a) * (halfPerimeter - this.b) * (halfPerimeter - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        console.log("Ошибка! Треугольник не существует");
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
+
+
+
 
 
