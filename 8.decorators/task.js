82e0af801f18697343d5825401a24ec51d98b693
i.shatunova <ivashatunova@gmail.com>
2023-08-08 11:18:10 +0300

    Second task done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -15,51 +15,45 @@ function cachingDecoratorNew(func) {
         }
         const result = func(...args); // в кеше результата нет — придётся считать
         cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
-        if (cache.length > 5) { 
+        if (cache.length > 5) {
             cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый)    
         }
         console.log("Вычисляем: " + result);
         return "Вычисляем: " + result;
     }
-    
+
 }
-
-// cache = [
-//     { hash: "7f49b84d0bbc38e96493718013baace6", value: 60 },
-//     { hash: "36d9d8df7a0a21c339bf74e2a30d68bd", value: 6 },
-//     { hash: "fd526d0a3bfd3ebdc1fc0f998d241da6", value: 791 },
-// ];
-
-
-const addAndMultiply = (a, b, c) => (a + b) * c;
-const upgraded = cachingDecoratorNew(addAndMultiply);
-upgraded(1, 2, 3); // вычисляем: 9
-upgraded(1, 2, 3); // из кеша: 9
-upgraded(2, 2, 3); // вычисляем: 12
-upgraded(3, 2, 3); // вычисляем: 15
-upgraded(4, 2, 3); // вычисляем: 18
-upgraded(5, 2, 3); // вычисляем: 21
-upgraded(6, 2, 3); // вычисляем: 24 (при этом кеш для 1, 2, 3 уничтожается)
-upgraded(1, 2, 3); // вычисляем: 9  (снова вычисляем, кеша нет)
-
-
-let add2 = (a, b) => a + b;
-    let multiply3 = (a, b, c) => a * b * c;
-    let upgAdd2;
-    let upgMultiply3;
-
-
-
-    upgAdd2 = cachingDecoratorNew(add2);
-    upgMultiply3 = cachingDecoratorNew(multiply3);
-
-    expect(upgAdd2(1, 2)).toEqual("Вычисляем: 3");
-    expect(upgAdd2(1, 2)).toEqual("Из кеша: 3");
-    expect(upgAdd2(1, 2)).toEqual("Из кеша: 3");
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
 
+    let timeoutId;
+    let count = 0; // количество апли / количество вызовов декорированной функции
+    let allCount = 0; // количество всех оберток / количество вызовов декоратора
+    wrapper.allCount = allCount;
+    wrapper.count = count;
+
+    function wrapper(...args) {
+        allCount++;
+        if (timeoutId) {
+            console.log("удалили текущий таймаут");
+            clearTimeout(timeoutId);
+        }
+        if (!timeoutId) {
+            func.apply(this, args);
+            count++;
+            wrapper.count = count;
+        }
+
+        console.log("создаем новый таймаут");
+        timeoutId = setTimeout(() => {
+            count++;
+            func.apply(this, args);
+            console.log("вызвали колбек");
+            wrapper.count = count;
+        }, delay);
+        wrapper.allCount = allCount;
+    }
+
+    return wrapper;
 }
-
-
